c717784d84474272e661fae1abf45ebde46892ff
Hashim Al-Ghamdi <gashem10@hotmail.com>
2021-05-25 15:18:09 +0300

    Cleaning up project

--- sary/src/router/index.js
+++ sary/src/router/index.js
@@ -1,21 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import AllHeros from '../views/AllHeros.vue'
+import HeroProfile from '../views/HeroProfile.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'All Heros',
+    component: AllHeros
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    path: '/HeroProfile',
+    name: 'Hero Profile',
+    component: HeroProfile
   }
 ]
 
